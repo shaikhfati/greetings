e4df9e613f5ee55e166e1a125107e0d90467795f
shaikhfati <fatima@qmed.ngo>
2022-08-17 15:35:37 +0530

    Display greetings to user

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_greetings
+ * @category    string
+ * @copyright   2022 Fatima Shaikh<fatima@qmed.ngo>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title($SITE->fullname);
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+echo $OUTPUT->header();
+if (isloggedin()) {
+    echo '<h2>Greetings, '.fullname($USER).'</h2>';
+} else {
+    echo '<h2>Greetings, user </h2>';
+}
+
+echo $OUTPUT->footer();
